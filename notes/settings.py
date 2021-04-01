258dfec2d716eadf9b20e0daad80e49cac0bec69
Aastik <aastikyadav1997.ay@gmail.com>
2021-04-01 21:18:42 +0530

    Image links changed to gh-pages

--- notes/settings.py
+++ notes/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
-from .aws.conf import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
